be31ee2df03e12a0d0a7097e7eda59d5e9d1e46b
grace-mugoiri <gracemugoiri@gmail.com>
2020-06-08 09:43:07 +0300

    setup Home and Dashboard files

--- hotel-app/src/App.js
+++ hotel-app/src/App.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import UserContainer from './components/UserContainer';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="container">
-			<div className="header">
-				<h2> Welcome to hotel finding. </h2>
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			loggedInStatus: "Not Logged In",
+			user: {}
+		}
+		this.handleLogin = this.handleLogin.bind(this);
+		this.handleLogout = this.handleLogout.bind(this);
+	}
+
+	checkLoginStatus() {
+		axios
+			.get("http://localhost:3000/", { withCredentials: true })
+			.then(response => {
+				if (
+					response.data.logged_in &&
+					this.state.loggedInStatus === 'NOT_LOGGED_IN'
+				) {
+					this.setState({
+						loggedInStatus: "LOGGED_IN",
+						user: response.data.user
+					});
+				} else if (
+					!response.data.logged_in &
+					(this.state.loggedInStatus === "LOGGED_IN")
+				) {
+					this.setState({
+						loggedInStatus: "NOT_LOGGED_IN",
+						user: {}
+					})
+				}
+			})
+			.catch(error => {
+				console.log("check loggin error", error)
+			})
+	}
+	componentDidMount() {
+		this.checkLoginStatus();
+	}
+
+	handleLogout() {
+		this.setState({
+			loggedInStatus: "NOT_LOGGED_IN",
+			user: {}
+		})
+	}
+
+	handleLogin(data) {
+		this.setState({
+			loggedInStatus: "Logged In",
+			user: data.user
+		})
+	}
+	render() {
+		return (
+			<div className="app">
+				<BrowserRouter>
+					<Switch>
+						<Route
+							exact
+							path={"/"}
+							render={props => (
+								<Home {...props}
+									handleLogin={this.handleLogin}
+									handleLogout={this.handleLogout}
+									loggedInStatus={this.state.loggedInStatus} />
+							)} />
+						<Route
+							exact
+							path={"/dashboard"}
+							render={props => (
+								<Dashboard {...props} loggedInStatus={this.state.loggedInStatus} />
+							)} />
+					</Switch>
+				</BrowserRouter>
 			</div>
-			<UserContainer />
-    </div>
-  );
+		);
+	}
 }
-
-export default App;
---
+++ hotel-app/src/components/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import SignUp from './registration/SignUp';
+import Login from './registration/Login';
+import axios from 'axios';
+// import './Home.css'
+
+export default class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+		this.handleLogoutClick = this.handleLogoutClick.bind(this);
+	}
+
+	handleSuccessfulAuth(data) {
+		this.props.handleLogin(data);
+		this.props.history.push('/dashboard');
+	}
+
+	handleLogoutClick() {
+		axios
+			.delete("http://localhost:3001/logout", { withCredentials: true })
+			.then(response => {
+				this.props.handleLogout();
+			})
+			.catch(error => {
+				console.log("logout error", error);
+			})
+	}
+	render() {
+		return (
+			<div className="Home">
+				<h2>Home</h2>
+				<h2>Status: {this.props.loggedInStatus}</h2>
+				<button onClick={() => this.handleLogoutClick()}>Logout</button>
+				<SignUp handleSuccessfulAuth={this.handleSuccessfulAuth} />
+				<Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
+			</div>
+		)
+	}
+}
