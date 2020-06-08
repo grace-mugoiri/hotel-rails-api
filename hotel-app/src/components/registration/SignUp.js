f64e8e38275152f9ef5019d31d2913115352d8b2
grace-mugoiri <gracemugoiri@gmail.com>
2020-06-08 09:46:53 +0300

    add SignUp form

---
+++ hotel-app/src/components/registration/SignUp.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Registration extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "",
+			phoneNumber: "",
+			registrationErrors: ""
+		}
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit(event) {
+		const {
+			name,
+			phoneNumber
+		} = this.state;
+
+		axios.post("http://localhost:3000/users", {
+			user: {
+				name: name,
+				phoneNumber: phoneNumber,
+			}
+		},
+			{ withCredentials: true }
+		)
+			.then(response => {
+				if (response.status === 200) {
+					this.props.handleSuccessfulAuth(response.data);
+				}
+
+			})
+			.catch(error => {
+				console.log("reg error", error);
+			})
+		event.preventDefault();
+	}
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input type="name"
+						name="name"
+						placeholder="name"
+						value={this.state.name}
+						onChange={this.handleChange} required
+					/>
+					<input type="phoneNumber"
+						name="phoneNumber"
+						placeholder="phoneNumber"
+						value={this.state.phoneNumber}
+						onChange={this.handleChange} required
+					/>
+
+					<button type='submit'>Register</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+
