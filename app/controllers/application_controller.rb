035f5caf7f45ca39f01ba41d05fc6072a20bba8a
grace-mugoiri <gracemugoiri@gmail.com>
2020-06-27 15:08:53 +0300

    add helper method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+	include ActionController::Helpers
+
+	helper_method :current_user
+
+	private
+
+	def current_user
+		@curent_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
 end
